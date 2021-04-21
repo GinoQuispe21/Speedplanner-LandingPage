4c1e71f8562a60acdb31208437d9503810a34984
Juan Alonso <leyva.calle1403@gmail.com>
2021-04-21 01:00:47 -0500

    :construction: basic html and css Feature added

--- js/main.js
+++ js/main.js
@@ -13,4 +13,22 @@ $(document).ready(function(){
             }
         }
     });
+    /*Features Carousel*/
+    $('.features-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+           
+        }
+    });
 });
