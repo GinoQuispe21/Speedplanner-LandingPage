e159c05517bbadd4f0d03e8d4860360700d76717
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-05-01 13:32:28 -0500

    Finished feature section and screenshots section :art: :zap:

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ $(document).ready(function(){
     $('.features-carousel').owlCarousel({
         loop:true,
         margin:0,
+        autoplay: true,
         responsiveClass:true,
         responsive:{
             0:{
@@ -27,6 +28,26 @@ $(document).ready(function(){
             },
             1000:{
                 items:3,
+            }
+           
+        }
+    });
+    
+    /*Screenshots Carousel*/
+    $('.screenshots-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay: true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:4,
             }
            
         }
